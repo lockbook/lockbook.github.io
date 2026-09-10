92517cfa640afae58831aaad2c91def541e730d0
parth <parth@mehrotra.me>
2026-09-10 17:21:39 -0400

    staging build @ 63bf094fc-dirty

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function () {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (keys) { return Promise.all(keys.map(function (k) { return caches.delete(k); })); })
+      .then(function () { return self.registration.unregister(); })
+      .then(function () { return self.clients.matchAll({ type: 'window' }); })
+      .then(function (clients) { clients.forEach(function (c) { c.navigate(c.url); }); })
+  );
+});
